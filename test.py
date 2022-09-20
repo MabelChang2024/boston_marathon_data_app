96325da4a6064035e1c04d7c72daba704326677c
TeamXsell <mabelchang2024>
2022-09-20 15:15:39 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import streamlit as st
+#import pandas as pd #st/ pd can be replaced with anything 
+
+st.title("Hello World!")
+st.header("Hello World!")
+st.write("Hello World!")
+st.write("Remove Hello World")
